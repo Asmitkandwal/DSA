f17d7176b53f8c602b447b467007a0c9cbf41f7e
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-04-17 08:21:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count numbers/count-numbers.py
+++ Difficulty: Easy/Count numbers/count-numbers.py
@@ -1,22 +1,21 @@
-#User function Template for python3
-
 class Solution:
-    def count (self, N):
+    def count(self, N):
         c = 0
         
-        for i in range(1,N+1):
+        for i in range(1, N + 1):
             temp = i
             isthree = False
-            while temp!=0 :
-                digit = 0
-                digit = temp%10
+            
+            while temp != 0:
+                digit = temp % 10
+                
                 if digit == 3:
                     isthree = True
                     break
-                temp = temp//10
+                
+                temp = temp // 10   # ✅ FIX HERE
             
-            if isthree==False :
+            if not isthree:
                 c += 1
                 
-        
-        return c        
+        return c
