0c4f20fd2f4b7f975fc57123f35cf00f8b49c426
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-04-17 12:08:59 +0530

    Time: 12 ms (89.52%) | Memory: 12.3 MB (57.87%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def reverse(self, x):
         INT_MAX = 2**31 - 1   # max 32-bit int
+        INT_MIN = -2**31
 
         sign = -1 if x < 0 else 1   # store sign
         x = abs(x)                  # work with positive
@@ -14,7 +15,7 @@ class Solution(object):
         rev *= sign   # restore sign
 
         # check 32-bit range
-        if rev < -2**31 or rev > INT_MAX:
+        if rev < INT_MIN or rev > INT_MAX:
             return 0
 
         return rev
