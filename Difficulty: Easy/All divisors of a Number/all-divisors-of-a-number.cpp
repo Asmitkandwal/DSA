06460d2e206814abe39fdaed18334fcec8c63213
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-04-19 11:04:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.cpp
+++ Difficulty: Easy/All divisors of a Number/all-divisors-of-a-number.cpp
@@ -1,21 +1,29 @@
 class Solution {
-  public:
+public:
     void print_divisors(int n) {
-        vector <int> v;
-        for(int i = 1;i*i<=n;i++){
-            if(n%i == 0){
+        vector<int> v;
+
+        // Instead of checking till n, we only go till sqrt(n)
+        // because divisors always come in pairs: (i , n/i)
+        for (int i = 1; i * i <= n; i++) {
+
+            if (n % i == 0) {
+
+                // First divisor of the pair
                 v.push_back(i);
-                
-                if((n/i) != i){
-                    v.push_back(n/i);
+
+                // Second divisor of the pair
+                // Avoid duplicate when n is a perfect square
+                if ((n / i) != i) {
+                    v.push_back(n / i);
                 }
             }
-            
-            
         }
-        
-        sort(v.begin(),v.end());
-        for(auto i : v){
+
+        // Pairs are not inserted in sorted order, so sort at end
+        sort(v.begin(), v.end());
+
+        for (auto i : v) {
             cout << i << " ";
         }
     }
