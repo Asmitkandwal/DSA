09d90d1433ef5a2dd9145bec92e2e50f8092face
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-04-21 09:56:59 +0530

    Time: 13 ms (31.53%) | Memory: 7.8 MB (37.79%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n < 2) return n;
-        return fib(n-1)+fib(n-2);
+
+        // Base case: fib(0)=0, fib(1)=1
+        if (n < 2) return n;
+
+        // Fibonacci number = sum of previous two terms
+        return fib(n - 1) + fib(n - 2);
     }
 };
