bad431b2ec01288faa95eed288ccf889b5145cb0
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-04-22 09:42:15 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (23.85%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
-        unordered_map<char,int> mpp;
+
+        unordered_map<char, int> mpp;
         int vowel = 0;
         int consonent = 0;
-        for(int i = 0;i<s.length();i++){
-            mpp[s[i] - 'a']++;
-            int temp = mpp[s[i]-'a'] ;
-            if(s[i] == 'a'||s[i] == 'e'||s[i] == 'i'||s[i] == 'o'||s[i] == 'u'){
-                vowel = max(temp,vowel);
+
+        for (int i = 0; i < s.length(); i++) {
+
+            // Update frequency of current character
+            mpp[s[i]]++;
+
+            int temp = mpp[s[i]];   // current character frequency
+
+            // Track highest frequency among vowels
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||
+                s[i] == 'o' || s[i] == 'u') {
+
+                vowel = max(temp, vowel);
             }
+
+            // Track highest frequency among consonants
             else {
-                consonent = max(temp,consonent);
+                consonent = max(temp, consonent);
             }
         }
-        return (vowel+consonent);
 
+        // Required answer = max vowel freq + max consonant freq
+        return vowel + consonent;
     }
 };
