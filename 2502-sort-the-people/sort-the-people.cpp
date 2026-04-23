5931bcdc27ef4b384bfb8aee94837be22ea27535
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-04-23 12:06:44 +0530

    Time: 20 ms (24.32%) | Memory: 27.9 MB (66.44%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        unordered_map<int,string> mpp;
-        for(int i = 0;i<heights.size();i++){
-            mpp.insert({heights[i],names[i]});
+
+        unordered_map<int, string> mpp;
+
+        // Store mapping: height -> corresponding name
+        for (int i = 0; i < heights.size(); i++) {
+            mpp[heights[i]] = names[i];
         }
-        sort(heights.begin(),heights.end());
-        reverse(heights.begin(),heights.end());
+
+        // Sort heights in increasing order first
+        sort(heights.begin(), heights.end());
+
+        // Reverse to get tallest to shortest order
+        reverse(heights.begin(), heights.end());
 
         vector<string> new_names;
-        for(int i = 0;i<heights.size();i++){
+
+        // Use sorted heights to fetch names in required order
+        for (int i = 0; i < heights.size(); i++) {
             new_names.push_back(mpp[heights[i]]);
         }
+
         return new_names;
-
-
     }
 };
