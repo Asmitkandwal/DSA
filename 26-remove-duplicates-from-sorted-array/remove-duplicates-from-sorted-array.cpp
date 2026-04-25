b8c7f7f2110583b7e6a3eede7c9963b10907fc8a
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-04-25 09:08:29 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (51.59%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-       int  index = 1;
 
-        for(int i = 1;i<nums.size();i++){
-            if(nums[i] != nums[i-1]){
+        // First element is already unique,
+        // next unique element will be placed from index 1
+        int index = 1;
+
+        for (int i = 1; i < nums.size(); i++) {
+
+            // If current element differs from previous,
+            // it means a new unique number is found
+            if (nums[i] != nums[i - 1]) {
+
+                // Place unique element in compact front portion
                 nums[index] = nums[i];
                 index++;
             }
         }
 
+        // Number of unique elements stored in front
         return index;
     }
 };
