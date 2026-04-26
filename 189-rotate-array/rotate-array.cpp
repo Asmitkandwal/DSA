35dfdcd0ccdfb97b5fcd6e5036dc6b2241e79b9f
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-04-26 19:23:34 +0530

    Time: 0 ms (100.00%) | Memory: 30.5 MB (18.12%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+
         int n = nums.size();
+
+        // Create vector with size n because we already know
+        // exact positions where elements must be placed.
+        // If it were empty + push_back(), elements would only
+        // be added at end, not directly inserted at index.
         vector<int> demo(n);
-        
-        for(int i = 0;i<n;i++){
-            int index = (i+k)%n;
+
+        for (int i = 0; i < n; i++) {
+
+            // New index after right rotation
+            int index = (i + k) % n;
+
+            // Direct assignment needs valid pre-sized indexes
             demo[index] = nums[i];
         }
 
+        // Replace original array with rotated array
         nums = demo;
-        
     }
 };
