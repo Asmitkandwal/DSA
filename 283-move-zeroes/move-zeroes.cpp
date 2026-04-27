cd680205632216af62eec5e6fdfb09fef2f56f42
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-04-27 09:05:35 +0530

    Time: 4 ms (10.39%) | Memory: 25 MB (5.25%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+
         vector<int> temp;
         int c = 0;
         int n = nums.size();
-        for(int i  = 0;i<n;i++){
-            if(nums[i] != 0) temp.push_back(nums[i]);
+
+        for (int i = 0; i < n; i++) {
+
+            // Keep non-zero elements in same relative order
+            if (nums[i] != 0)
+                temp.push_back(nums[i]);
+
+            // Count zeros to place later at end
             else
-            c++;
+                c++;
         }
 
-        for (int i = 0;i<c;i++){
+        // Append all counted zeros at the end
+        for (int i = 0; i < c; i++) {
             temp.push_back(0);
         }
-        
-        nums = temp;
 
+        // Replace original array with required arrangement
+        nums = temp;
     }
 };
