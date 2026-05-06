f3f2561551c80d5eeb208a0db77406100b585f8a
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-05-06 08:27:40 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (57.73%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -3,7 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         int xor_val = 0;
         for(int i = 0;i<nums.size();i++){
-            xor_val = xor_val xor nums[i];
+            xor_val = xor_val ^ nums[i];
         }
 
         return xor_val;
