5fc1b4e2378cdb7c2523e11077928352e4d10ee4
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-05-06 08:28:35 +0530

    Time: 3 ms (22.99%) | Memory: 20.6 MB (57.73%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,11 +1,20 @@
+// OPTIMAL APPROACH USING XOR
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+
         int xor_val = 0;
-        for(int i = 0;i<nums.size();i++){
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            // Same numbers cancel each other in XOR
+            // a ^ a = 0
+            // 0 ^ x = x
             xor_val = xor_val ^ nums[i];
         }
 
+        // Only unique element remains after all cancellations
         return xor_val;
     }
 };
