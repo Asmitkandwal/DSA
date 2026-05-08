72718314b47db30747a63eb5c2f5fadd3f375eaa
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-05-08 10:35:59 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (10.66%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,23 +1,40 @@
+// OPTIMAL APPROACH (DUTCH NATIONAL FLAG)
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+
         int n = nums.size();
-        int low = 0;
-        int mid = 0;
-        int high = n-1;
-        while(mid<=high){
-            if(nums[mid] == 0){
-                swap(nums[low],nums[mid]);
+
+        int low = 0;       // next position for 0
+        int mid = 0;       // current element being processed
+        int high = n - 1;  // next position for 2
+
+        while (mid <= high) {
+
+            // Place 0 in left section
+            if (nums[mid] == 0) {
+
+                swap(nums[low], nums[mid]);
+
                 low++;
                 mid++;
             }
-            else if(nums[mid] == 1){
+
+            // 1 is already in correct middle section
+            else if (nums[mid] == 1) {
                 mid++;
             }
 
-            else{
-                swap(nums[mid],nums[high]);
+            // Place 2 in right section
+            else {
+
+                swap(nums[mid], nums[high]);
+
                 high--;
+
+                // mid is not increased here because
+                // swapped element from high is still unchecked
             }
         }
     }
