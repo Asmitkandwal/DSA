7c758c8377e612bfbaf995fdc3a2b3faad610656
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-05-08 10:29:33 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (10.66%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,28 +1,44 @@
+// BRUTE FORCE APPROACH
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+
         vector<int> vec;
+
         int zeros = 0;
         int ones = 0;
         int twos = 0;
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i] == 0) zeros++;
-            else if(nums[i] == 1) ones++;
+
+        // Count frequency of 0s, 1s and 2s
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (nums[i] == 0)
+                zeros++;
+
+            else if (nums[i] == 1)
+                ones++;
+
             else
-            twos++;
+                twos++;
         }
 
-
-        for(int i = 0;i<zeros;i++){
+        // Insert all 0s first
+        for (int i = 0; i < zeros; i++) {
             vec.push_back(0);
         }
-        for(int i = 0;i<ones;i++){
+
+        // Then insert all 1s
+        for (int i = 0; i < ones; i++) {
             vec.push_back(1);
         }
-        for(int i = 0;i<twos;i++){
+
+        // Finally insert all 2s
+        for (int i = 0; i < twos; i++) {
             vec.push_back(2);
         }
 
+        // Replace original array with sorted result
         nums = vec;
     }
 };
