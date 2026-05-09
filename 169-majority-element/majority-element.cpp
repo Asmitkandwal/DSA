2b48c8b1320840413848d629f67623dd67518f84
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-05-09 15:50:24 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (27.26%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,14 +1,21 @@
+// BETTER APPROACH USING HASH MAP
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int,int> mpp;
+
+        map<int, int> mpp;
         int n = nums.size();
-        for(int i = 0;i<n;i++){
+
+        // Store frequency of each element
+        for (int i = 0; i < n; i++) {
             mpp[nums[i]]++;
         }
 
-        for(auto i : mpp){
-            if(i.second > (n/2) ){
+        // Find element appearing more than n/2 times
+        for (auto i : mpp) {
+
+            if (i.second > (n / 2)) {
                 return i.first;
             }
         }
