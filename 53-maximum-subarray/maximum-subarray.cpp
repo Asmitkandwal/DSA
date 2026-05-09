a766b67afcbfee179e447e43f592e1c469017ce3
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-05-09 18:49:34 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (51.36%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -3,9 +3,7 @@ public:
     int maxSubArray(vector<int>& nums) {
         int maximum_value = INT_MIN;
         int n = nums.size();
-        if (n==1){
-            return nums[0];
-        }
+        
          int current_sum = 0;
         for(int i = 0;i<n;i++){
             
