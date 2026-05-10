a4a430f006e443f6c552c61c44d98d9480e2a7f6
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-05-10 05:34:29 +0530

    Time: 0 ms (100.00%) | Memory: 97.3 MB (59.79%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,17 +1,27 @@
-//BRUTEFORCE
+// BRUTEFORCE
 
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+
+        // Track minimum buying price seen so far
         int minimum_value = prices[0];
+
         int maxProfit = 0;
 
-        for(int i = 0;i<prices.size();i++){
-           if(prices[i] > minimum_value){
-            int cost =  prices[i]-minimum_value;
-            maxProfit = max(maxProfit ,cost);
-           }
-           minimum_value = min(minimum_value , prices[i]);
+        for (int i = 0; i < prices.size(); i++) {
+
+            // Profit if sold today after buying at minimum price
+            if (prices[i] > minimum_value) {
+
+                int cost = prices[i] - minimum_value;
+
+                // Store maximum profit possible
+                maxProfit = max(maxProfit, cost);
+            }
+
+            // Update minimum buying price for future days
+            minimum_value = min(minimum_value, prices[i]);
         }
 
         return maxProfit;
