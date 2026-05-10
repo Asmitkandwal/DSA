4ac5e7121da1259bee46624d0b69f50b538f4b3a
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-05-10 06:32:45 +0530

    Time: 0 ms (100.00%) | Memory: 139.1 MB (17.03%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,23 +1,33 @@
+// BRUTE FORCE APPROACH
+
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
+
         vector<int> positive;
         vector<int> negative;
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i] > 0){
-            positive.push_back(nums[i]);
+
+        // Separate positive and negative numbers
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (nums[i] > 0) {
+                positive.push_back(nums[i]);
             }
 
-            else{
-            negative.push_back(nums[i]);
+            else {
+                negative.push_back(nums[i]);
             }
-            
         }
 
         int n = nums.size();
-        for(int i = 0;i<n/2;i++){
-            nums[i*2] = positive[i];
-            nums[(i*2)+1] = negative[i];
+
+        // Place positives at even indexes
+        // and negatives at odd indexes
+        for (int i = 0; i < n / 2; i++) {
+
+            nums[i * 2] = positive[i];
+
+            nums[(i * 2) + 1] = negative[i];
         }
 
         return nums;
