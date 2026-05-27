ad05c908d0983b6d9b6d0cfb5c35dc5e81507f2c
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-05-27 11:54:01 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (7.38%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        vector<vector<int>>arr(n,vector<int>(n,0));
 
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
-                arr[j][n-i-1] = matrix[i][j];
+        int n = matrix.size();
+
+        // Temporary matrix is used because directly modifying
+        // original matrix would overwrite values still needed later
+        vector<vector<int>> arr(n, vector<int>(n, 0));
+
+        // Place every element into its 90° rotated position
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                // Mapping:
+                // matrix[i][j] -> arr[j][n-i-1]
+                arr[j][n - i - 1] = matrix[i][j];
             }
         }
 
-        for(int i = 0;i<n;i++){
-            for(int j= 0;j<n;j++){
+        // Copy rotated matrix back into original matrix
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+
                 matrix[i][j] = arr[i][j];
             }
         }
