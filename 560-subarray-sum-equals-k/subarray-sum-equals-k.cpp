e61a1e79db078d9b644f72b845301c80bda84eef
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-05-30 11:41:48 +0530

    Time: 43 ms (56.75%) | Memory: 45.5 MB (55.43%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,20 +1,34 @@
+// OPTIMAL APPROACH (PREFIX SUM + HASH MAP)
+
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int sum = 0 , count = 0;
+
+        int sum = 0, count = 0;
         int n = nums.size();
 
-        unordered_map<int,int> PrefixSum;
-        PrefixSum[0] =1;
+        unordered_map<int, int> PrefixSum;
 
-        for(int i = 0;i<n;i++){
+        // Handles case when subarray starting from index 0 has sum k
+        PrefixSum[0] = 1;
+
+        for (int i = 0; i < n; i++) {
+
+            // Running prefix sum till current index
             sum += nums[i];
 
-            int val = sum-k;
-            if(PrefixSum.find(val) != PrefixSum.end()){
+            // If a previous prefix sum of (sum - k) exists,
+            // then subarray between those positions has sum k
+            int val = sum - k;
+
+            if (PrefixSum.find(val) != PrefixSum.end()) {
+
+                // Add all occurrences because multiple prefix sums
+                // can generate valid subarrays
                 count = count + PrefixSum[val];
             }
-            
+
+            // Store current prefix sum frequency
             PrefixSum[sum]++;
         }
 
