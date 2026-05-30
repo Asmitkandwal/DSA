ecf4795c311bffddd13b42fb699e212efc663c5a
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-05-30 11:15:17 +0530

    Time: 2478 ms (12.20%) | Memory: 34.4 MB (82.67%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,17 +1,28 @@
+// BRUTE FORCE APPROACH
+
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+
         int n = nums.size();
         int count = 0;
-        for(int i = 0;i<n;i++){
+
+        // Consider every index as starting point of a subarray
+        for (int i = 0; i < n; i++) {
+
             int sum = 0;
-            for(int j = i;j<n;j++){
+
+            // Extend subarray one element at a time
+            for (int j = i; j < n; j++) {
+
                 sum += nums[j];
-                if(sum == k){
-                count++;
+
+                // If current subarray sum equals k,
+                // we found one valid subarray
+                if (sum == k) {
+                    count++;
+                }
             }
-            }
-            
         }
 
         return count;
