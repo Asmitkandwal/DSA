aeffee42aa04aa50c741bb35cea7b45bc04c5aad
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-06-03 11:22:40 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (62.45%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int count1 = 0;
+        int count2 = 0;
+        int el1 = INT_MIN;
+        int el2 = INT_MIN;
         int n = nums.size();
-        vector<int> ans;
-        for(int i = 0;i<n;i++){
-
-            if(find(ans.begin(),ans.end(),nums[i]) != ans.end() ){
-                continue;
+        for(int i  = 0;i<n;i++){
+            if(count1 == 0 && nums[i] != el2){
+                count1++;
+                el1 = nums[i];
+            }
+            else if(count2 == 0 && nums[i] != el1){
+                count2++;
+                el2 = nums[i];
+            }
+            else if(nums[i] == el1)  { count1++; }
+            else if(nums[i] == el2)  { count2++; }
+            else{
+                count1--;
+                count2--;
             }
 
-            int count = 0;
-            for(int j = 0;j<n;j++){
-                if(nums[i] == nums[j]){
-                    count++;
-                }
-            }
-
-            if (count > n/3){
-            ans.push_back(nums[i]);
-            }
         }
+
+        vector<int> ans;
+        count1 = 0;
+        count2 = 0;
+
+        for(int i = 0;i<n;i++){
+            if(el1 == nums[i]) count1++;
+            if(el2 == nums[i]) count2++;
+        }
+        int min = (n/3);
+        if(count1 > min) ans.push_back(el1);
+        if(count2 > min) ans.push_back(el2);
 
         return ans;
     }
