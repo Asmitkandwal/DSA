0be85c206445c30ad1acb9eb519c1dacc952bcae
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-06-06 22:07:35 +0530

    Time: 51 ms (43.77%) | Memory: 29 MB (91.69%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,37 +1,52 @@
+//OPTIMAL SOLUTION(ONLY WORKING)
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
         int n = nums.size();
-        for(int i = 0;i<n;i++){
+        
+        // 1. Sort the numbers from smallest to largest.
+        // This lets us use two pointers and easily skip duplicate answers.
+        sort(nums.begin(), nums.end());
+        
+        for(int i = 0; i < n; i++){
+            // If this number is the same as the last one we processed, skip it.
+            // This prevents us from creating the exact same triplet again.
             if(i > 0 && nums[i] == nums[i-1]) continue;
 
-            int j = i+1;
-            int k = n-1;
-            while(j<k){
-                int sum = (nums[i] + nums[j] + nums[k]);
+            // 2. Set up two pointers for the rest of the array.
+            int j = i + 1; // Starts right after the fixed number
+            int k = n - 1; // Starts at the very end of the array
+            
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                
+                // Case 1: The sum is exactly 0. We found a valid triplet!
                 if(sum == 0){
-                    vector<int> temp = {nums[i],nums[j],nums[k]};
-                    ans.push_back(temp);
-                    //ans.push_back({nums[i],nums[j],nums[k]})
+                    ans.push_back({nums[i], nums[j], nums[k]});
 
-                    while(j<k && nums[j] == nums[j+1]) j++;
-                    while(j<k && nums[k] == nums[k-1]) k--; 
+                    // Skip identical numbers on the left and right 
+                    // so we don't pick up duplicate combinations.
+                    while(j < k && nums[j] == nums[j+1]) j++;
+                    while(j < k && nums[k] == nums[k-1]) k--; 
 
+                    // Move both pointers inward to keep looking for other pairs
                     j++;
                     k--;
                 }
-
+                // Case 2: The sum is too big. 
+                // Since the array is sorted, move the right pointer leftward to get a smaller number.
                 else if(sum > 0){
                     k--;
                 }
-                else{
+                // Case 3: The sum is too small.
+                // Move the left pointer rightward to get a bigger number.
+                else {
                     j++;
                 }
             }
         }
-
         return ans;
     }
 };
