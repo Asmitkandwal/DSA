0cacf65554960c8ff505aefa1db988ee97f492cd
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-06-10 11:12:54 +0530

    Time: 23 ms (54.69%) | Memory: 17.4 MB (82.44%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,43 +1,59 @@
+//ABSOLUTE OPTIMAL CODE..
+
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
         vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        for(int i = 0;i<n;i++){
-            if(i>0 && nums[i] == nums[i-1]) continue;
+        
+        // Edge Case: If there are fewer than 4 numbers, a quadruplet is impossible.
+        if (n < 4) return ans; 
 
-            for(int j = i+1;j<n;j++){
-                if(j-i > 1 && nums[j] == nums[j-1]) continue;
+        // 1. Sort the array so we can use Two-Pointers and easily skip duplicates
+        sort(nums.begin(), nums.end());
 
-                int k = j+1;
-                int l = n-1;
+        // Loop 1: Fixes the 1st number
+        for (int i = 0; i < n; i++) {
+            // Skip duplicates for the 1st number to avoid duplicate answers
+            if (i > 0 && nums[i] == nums[i-1]) continue;
 
-                while(k<l){
-                long long sum = (long long )nums[i]+nums[j]+nums[k]+nums[l];
-                if(sum == target){
-                    vector<int> temp = {nums[i],nums[j],nums[k],nums[l]};
-                    sort(temp.begin(),temp.end());
-                    ans.push_back(temp);
+            // Loop 2: Fixes the 2nd number
+            for (int j = i + 1; j < n; j++) {
+                // Skip duplicates for the 2nd number.
+                // 'j > i + 1' ensures we don't accidentally skip the first choice for 'j'
+                if (j > i + 1 && nums[j] == nums[j-1]) continue;
 
-                    k++;
-                    l--;
+                // 2. Set up Two-Pointers for the remaining sorted window
+                int k = j + 1; // 3rd pointer (moves right)
+                int l = n - 1; // 4th pointer (moves left)
 
-                    while(k<l && nums[k] == nums[k-1]) k++;
-                    while(k<l && nums[l] == nums[l+1]) l--;
+                while (k < l) {
+                    // Use 'long long' cast on the first element to force C++ 
+                    // to calculate the entire sum without 32-bit integer overflow
+                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
 
-                    
-                }
-                else if(sum > target){
-                    l--;
-                }
-                else{
-                    k++;
+                    // Case 1: Sum matches target
+                    if (sum == target) {
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+
+                        // Skip identical adjacent numbers to avoid duplicate answers
+                        while (k < l && nums[k] == nums[k + 1]) k++;
+                        while (k < l && nums[l] == nums[l - 1]) l--;
+
+                        // Move both pointers inward to look for the next combination
+                        k++;
+                        l--;
+                    } 
+                    // Case 2: Sum is too big, move the right pointer leftward
+                    else if (sum > target) {
+                        l--;
+                    } 
+                    // Case 3: Sum is too small, move the left pointer rightward
+                    else {
+                        k++;
+                    }
                 }
             }
-            }
-
-
         }
         return ans;
     }
