e01e841f5646c3c8da18b47c88238b958ae73f2c
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-06-10 09:05:04 +0530

    Time: 1785 ms (5.01%) | Memory: 474.6 MB (5.03%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -5,20 +5,15 @@ public:
         set<vector<int>>st;
         for(int i = 0;i<n;i++){
             for(int j = i+1;j<n;j++){
-                unordered_set<int> hashset;
+                unordered_set<long long > hashset;
                 for(int k = j+1;k<n;k++){
                     long long sum = (long long)nums[i]+nums[j]+nums[k];
                     long long fourth = (long long)target - sum;
-                    // Check if fourth can safely fit inside a standard 32-bit int
-                    if (fourth >= INT_MIN && fourth <= INT_MAX) {
-                        // Now it is completely safe to cast it to an int
-                        int target_element = (int)fourth; 
-    
-                       if (hashset.find(target_element) != hashset.end()) {
-                            vector<int> temp = {nums[i], nums[j], nums[k], target_element};
-                           sort(temp.begin(), temp.end());
-                            st.insert(temp);
-                        }
+                    int new_fourth = (int)fourth;
+                    if(hashset.find(fourth) != hashset.end()){
+                        vector<int> temp = {nums[i],nums[j],nums[k],new_fourth};
+                        sort(temp.begin(),temp.end());
+                        st.insert(temp);
                     }
                     hashset.insert(nums[k]);
                 }
