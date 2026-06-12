b78c9160a09b63da99e27d92516836bc2a97a840
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-06-12 10:08:42 +0530

    Time: 3 ms (89.53%) | Memory: 23.8 MB (58.95%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -7,16 +7,19 @@ public:
         int i = 0;
         while(i<n){
             int start = intervals[i][0];
-            int end  = intervals[i][1];
+            int end = intervals[i][1];
+
             int j = i+1;
             while(j<n && intervals[j][0] <= end){
-                end = max(end,intervals[j][1]);
+                end = max(intervals[j][1],end);
                 j++;
             }
+
             ans.push_back({start,end});
+
             i = j;
         }
+
         return ans;
     }
-
 };
