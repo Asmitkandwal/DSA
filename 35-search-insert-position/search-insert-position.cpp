5d2f74aaf5058f06191b49565b3131109679022d
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-07-20 14:49:52 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (43.32%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -2,16 +2,22 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int low = 0;
-        int high = nums.size();
-        for(int i = 0;i<nums.size();i++){
-            int mid = (low + (high-low))/2;
-            if(nums[i] == target) return i;
-            if(nums[i] < target) {
+        int high = nums.size()-1;
+        int ans = nums.size();
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if (nums[mid] == target ){
+                return mid;
+            }
+            if (nums[mid] >= target){
+                high = mid-1;
+                ans = mid;
+            }
+            else{
                 low = mid+1;
             }
-            if(nums[i] > target) return i;
-            
+
         }
-        return nums.size();
+        return ans;
     }
 };
