b0a667219b67c9a0c1b6242c13897fb601cdd9ec
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-07-21 19:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (20.15%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,27 +1,50 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int first = -1,last = -1;
 
-        if(nums.empty()){
-            return {first,last};
-        }
-
-        if(nums.size()==1 && nums[0] == target){
-            first = 0, last = 0;
-            return {first,last};
-        }
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i] == target && first == -1){
-                first = i;
-            }else if(nums[i] == target) {
-                last = i;
+    int firstoccurence(vector<int> &nums , int target){
+        int low = 0, high = nums.size()-1;
+        int First = -1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] == target){
+                First = mid;
+                high = mid-1;
             }
+            else if(nums[mid] < target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }  
         }
-        if(first != -1 && last == -1){
-            last = first;
+        return First;
+    }
+
+    int lastoccurence(vector<int> &nums, int target){
+         int low = 0, high = nums.size()-1;
+        int Last = -1;
+        while(low <= high){
+            int mid = (high+low)/2;
+            if(nums[mid] == target){
+                Last = mid;
+                low = mid+1;
+            }
+            else if(nums[mid] < target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }  
         }
+        return Last;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = firstoccurence(nums,target);
+        if(first == -1) return {-1,-1};
+        int last = lastoccurence(nums,target);
 
         return{first,last};
+
     }
 };
