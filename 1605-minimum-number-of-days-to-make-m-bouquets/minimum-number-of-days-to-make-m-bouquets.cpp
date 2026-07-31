0d82a3876e5d6e0dc238b2951af7941eb9e0cd9d
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-07-31 10:56:21 +0530

    Time: 24 ms (42.36%) | Memory: 70.5 MB (80.22%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,48 +1,41 @@
 class Solution {
 public:
-    bool canMake(vector<int>& bloomDay, int day, int m, int k) {
-        int bouquets = 0;
-        int flowers = 0;
-
-        for (int i = 0; i < bloomDay.size(); i++) {
-
-            if (bloomDay[i] <= day) {
-                flowers++;
-            } else {
-                flowers = 0;
-            }
-
-            if (flowers == k) {
-                bouquets++;
-                flowers = 0;      // Flowers are used in one bouquet
-            }
-        }
-
-        return bouquets >= m;
-    }
-
     int minDays(vector<int>& bloomDay, int m, int k) {
+        int n = bloomDay.size();
 
-        // Impossible to make enough bouquets
-        if ((long long)m * k > bloomDay.size())
+        if ((long long)m * k > n)
             return -1;
 
         int low = *min_element(bloomDay.begin(), bloomDay.end());
         int high = *max_element(bloomDay.begin(), bloomDay.end());
 
-        int ans = -1;
-
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
-            if (canMake(bloomDay, mid, m, k)) {
-                ans = mid;
-                high = mid - 1;      // Try to find an even smaller day
+            int bouquets = 0;
+            int flowers = 0;
+
+            // Check if we can make m bouquets by day = mid
+            for (int i = 0; i < n; i++) {
+                if (bloomDay[i] <= mid) {
+                    flowers++;
+                } else {
+                    flowers = 0;
+                }
+
+                if (flowers == k) {
+                    bouquets++;
+                    flowers = 0; // Use these flowers
+                }
+            }
+
+            if (bouquets >= m) {
+                high = mid - 1;   // Try a smaller day
             } else {
-                low = mid + 1;       // Need to wait longer
+                low = mid + 1;    // Need more days
             }
         }
 
-        return ans;
+        return low;
     }
 };
