a7087173fbe8e0248b590473c947be18a61bc59f
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-08-01 16:18:28 +0530

    Time: 11 ms (39.49%) | Memory: 25.9 MB (98.61%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
     int smallestDivisor(vector<int>& nums, int threshold) {
+        // Range of possible divisors:
+        // Smallest possible divisor is 1
         int low = 1;
-        int high = *max_element(nums.begin(),nums.end());
+        // Largest useful divisor is the max number in the array
+        int high = *max_element(nums.begin(), nums.end());
         
-        int ans = INT_MAX;
-        while(low<=high){
-            int temp = 0;
-            int mid = low +(high-low)/2;
-            for(int i = 0;i<nums.size();i++){
-                int value = ceil((double)nums[i]/(double)mid);
-                temp += value;
-            }
-            
+        int ans = high;
 
-            if(temp <= threshold){
-                ans = min(ans,mid);
-                high = mid-1;
-            }else if(temp > threshold){
-                low = mid+1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int sum = 0;
+
+            // Divide every number by 'mid' (rounded up) and add to total sum
+            for (int num : nums) {
+                // INTEGER CEILING TRICK:
+                // (num + mid - 1) / mid rounds UP without slow double/ceil math
+                sum += (num + mid - 1) / mid;
+                //can be written as sum+= ceil((double)num /(double) mid)
             }
 
+            // DECISION RULE:
+            if (sum <= threshold) {
+                ans = mid;       // 'mid' worked! Save it as a valid answer.
+                high = mid - 1;  // Try to find an even SMALLER divisor on the left side.
+            } else {
+                low = mid + 1;   // Sum was too big! We need a LARGER divisor to make the sum smaller.
+            }
         }
 
         return ans;
