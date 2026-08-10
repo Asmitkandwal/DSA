32ff317c70b5758ad63bedeeaacfeb377aea0f6a
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-08-10 10:27:04 +0530

    Time: 0 ms (100.00%) | Memory: 24.6 MB (8.12%) - LeetSync

--- 3221-find-the-peaks/find-the-peaks.cpp
+++ 3221-find-the-peaks/find-the-peaks.cpp
@@ -1,12 +1,21 @@
+// FIND ALL PEAK ELEMENT INDICES
+
 class Solution {
 public:
     vector<int> findPeaks(vector<int>& mountain) {
+
         vector<int> ans;
-        for(int i= 1;i<mountain.size()-1;i++){
-            if(mountain[i]>mountain[i+1] && mountain[i]>mountain[i-1]){
+
+        // We skip first and last element
+        // because they cannot have two neighbors
+        for (int i = 1; i < mountain.size() - 1; i++) {
+
+            // A peak must be strictly greater than both neighbors
+            if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) {
                 ans.push_back(i);
             }
         }
+
         return ans;
     }
 };
