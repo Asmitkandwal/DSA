b718ca4aac4e25ec8fe602caa8ccb59f2a21e9d5
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-08-13 10:46:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (93.86%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,21 +1,30 @@
-//brute force
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        sort(strs.begin(),strs.end());
+        // If the vector of strings is empty, there is no common prefix
+        if (strs.empty()) return "";
 
         string ans = "";
-        string first = strs[0];
-        string last  = strs[strs.size()-1];
 
-        for(int i  = 0;i<first.size();i++){
-            if(first[i] == last[i]){
-                ans += first[i];
-            }else{
-                break;
+        // Loop character by character through the FIRST string (column by column)
+        for (int i = 0; i < strs[0].size(); i++) {
+            char ch = strs[0][i];
+
+            // Compare this character against the same column 'i' in all other strings
+            for (int j = 1; j < strs.size(); j++) {
+                
+                // STOP CONDITION:
+                // 1. Index 'i' goes past the end of string strs[j]
+                // 2. Character in strs[j] does not match character 'ch'
+                if (i >= strs[j].size() || ch != strs[j][i]) {
+                    return ans; // Return the common prefix found so far
+                }
             }
-        }
 
-        return ans;
+            // If all strings matched at index 'i', append character to answer
+            ans += ch;
+        } 
+
+        return ans; // Whole first string is the common prefix
     }
 };
