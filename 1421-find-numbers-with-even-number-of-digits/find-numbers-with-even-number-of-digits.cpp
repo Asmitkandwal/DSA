af8f329b0bf5314d309ff9cd31c31e9af3b2d3f6
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-08-15 21:27:56 +0530

    Time: 5 ms (1.20%) | Memory: 14.5 MB (9.69%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int findNumbers(vector<int>& nums) {
-        int val = 0;
+        int val = 0; // Counter for numbers with an even number of digits
         
-        for(int i = 0;i<nums.size();i++){
+        for (int i = 0; i < nums.size(); i++) {
+            // Convert the integer into a string to inspect character length
             string str = to_string(nums[i]);
-            if(str.length()%2 == 0){
+            
+            // Check if the total digit count (string length) is even
+            if (str.length() % 2 == 0) {
                 val++;
             }
         }
+
         return val;
     }
 };
