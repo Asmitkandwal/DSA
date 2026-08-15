202231f0ca5d3c0780037f2fc3b64e9dcaf1612e
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-08-15 10:25:28 +0530

    Time: 7 ms (20.46%) | Memory: 16.5 MB (11.91%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,35 +1,35 @@
-//OPTIMAL APPROACH
+//OPTIMAL SOLUTION
 
 class Solution {
 public:
     string frequencySort(string s) {
-
+        // STEP 1: Count frequency of each character
         unordered_map<char, int> mp;
-
-        // Count frequency
-        for(char c : s) {
+        for (char c : s) {
             mp[c]++;
         }
 
-        // Create buckets
+        // STEP 2: Create buckets where INDEX = FREQUENCY
+        // Max possible frequency of any character is s.size()
+        // So bucket array size is (s.size() + 1)
         vector<vector<char>> bucket(s.size() + 1);
 
-        // Put each character into its frequency bucket
-        for(auto it : mp) {
+        // Put each character into the bucket matching its frequency
+        // Example: If 'e' appears 2 times, put 'e' into bucket[2]
+        for (auto it : mp) {
             char c = it.first;
             int freq = it.second;
-
             bucket[freq].push_back(c);
         }
 
         string ans = "";
 
-        // Start from highest frequency
-        for(int freq = s.size(); freq >= 1; freq--) {
-
-            for(char c : bucket[freq]) {
-
-                for(int i = 0; i < freq; i++) {
+        // STEP 3: Iterate backwards from highest frequency (s.size()) down to 1
+        for (int freq = s.size(); freq >= 1; freq--) {
+            // For every character that has this frequency:
+            for (char c : bucket[freq]) {
+                // Append the character 'freq' times to the result string
+                for (int i = 0; i < freq; i++) {
                     ans += c;
                 }
             }
