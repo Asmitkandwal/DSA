3159f4d6d999a57f342019ea960f59febc9c7a3a
Asmit Kandwal <asmitkandwal17@gmail.com>
2026-09-20 10:02:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (90.51%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -11,21 +11,20 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        vector<int>mid_elements;
-        int count = 0;
-        ListNode*ptr = head;
-        while(ptr != nullptr){
-            count++;
-            ptr = ptr->next; 
+        // OPTIMAL APPROACH: Fast & Slow Pointers (Tortoise and Hare)
+        // Eliminates the need for a first pass to count nodes.
+        ListNode* slow = head; // Moves 1 step at a time
+        ListNode* fast = head; // Moves 2 steps at a time
+
+        // Loop continues until 'fast' hits the end of the list:
+        // - Even length: fast reaches nullptr
+        // - Odd length:  fast reaches the last node (fast->next is nullptr)
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;       // 1 step forward
+            fast = fast->next->next; // 2 steps forward
         }
 
-        int mid = count/2;
-        ptr = head;
-        while(mid){
-            ptr = ptr->next;
-            mid--;
-        }
-
-        return ptr;
+        // When fast reaches the end, slow is guaranteed to be at the middle node
+        return slow;
     }
 };
